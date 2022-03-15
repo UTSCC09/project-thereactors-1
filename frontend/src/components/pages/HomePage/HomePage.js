40702b40c4550cb965a1330f2cbe6ac41ce6c876
Daniel Xu <niellxu@yahoo.ca>
2022-03-15 13:27:45 -0400

    Test connection with the backend socket

--- frontend/src/components/pages/HomePage/HomePage.js
+++ frontend/src/components/pages/HomePage/HomePage.js
@@ -1,10 +1,16 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import io from 'socket.io-client';
+import config from '../../../environments'
 
 export default function HomePage() {
-    
-    useEffect(() => {
+    const [socket, setSocket] = useState(null);
 
-    }, []);
+    useEffect(() => {
+        const newSocket = io(config.backendUrl);
+        setSocket(newSocket);
+        console.log(newSocket);
+        return () => newSocket.close();
+    }, [setSocket]);
 
     return (
         <div>Home Page</div>
