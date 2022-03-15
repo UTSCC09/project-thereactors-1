629b9ce11e5d6d2168db2659a689940706b35908
Daniel Xu <niellxu@yahoo.ca>
2022-03-15 14:37:52 -0400

    Add test for graphql query

--- frontend/src/components/pages/HomePage/HomePage.js
+++ frontend/src/components/pages/HomePage/HomePage.js
@@ -5,20 +5,48 @@ import config from '../../../environments'
 export default function HomePage() {
     const [socket, setSocket] = useState(null);
     const [connected, setConnected] = useState(false);
+    const [graphqlSuccess, setGraphqlSuccess] = useState(false);
 
     useEffect(() => {
+        // Socket test
         const newSocket = io(config.backendUrl);
         newSocket.on("connect", () => {
             setConnected(true);
         })
         setSocket(newSocket);
+
+        // GraphQL test
+        const query = `
+            query {
+                getUsers {
+                    _id,
+                    firstName,
+                    lastName
+                }
+            }
+        `;
+        fetch(config.graphqlUrl, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+            },
+            body: JSON.stringify({ query }),
+        })
+        .then((response) => response.json())
+        .then((result) => {
+            console.log(result);
+            setGraphqlSuccess(true);
+        });
+
         return () => newSocket.close();
-    }, [setSocket, setConnected]);
+    }, [setSocket, setConnected, setGraphqlSuccess]);
 
     return (
         <div>
             Home Page
             {connected && <div>Socket connection success</div>}
+            { graphqlSuccess && <div>GraphQL query success</div> }
         </div>
     )
 }
--- frontend/src/environments.js
+++ frontend/src/environments.js
@@ -1,3 +1,4 @@
 export default {
   backendUrl: "http://localhost:3001",
+  graphqlUrl: "http://localhost:3001/api/graphql",
 }
