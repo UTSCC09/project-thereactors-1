2a2da4840230c2e940f20ce5d6d01792d2e8b6f2
Daniel Xu <niellxu@yahoo.ca>
2022-03-17 14:56:44 -0400

    Code format fix

--- backend/socket_handlers.js
+++ backend/socket_handlers.js
@@ -1,6 +1,6 @@
-import {saveMessage} from './chatroom_util.js';
+import { saveMessage } from './chatroom_util.js';
 import { verifyJwt } from './utils.js';
-import {Party, Message} from './db.js';
+import { Party, Message } from './db.js';
 
 const checkUserInvited = (username,roomid, callback) =>  {
   const query = Party.where({_id : roomid}).findOne((err,doc)=> {
@@ -9,8 +9,9 @@ const checkUserInvited = (username,roomid, callback) =>  {
     } else {
       callback(true);
     }
-  })
+  });
 }
+
 const addConnectedUser = (username,roomid) =>  {
   Party.where({_id : roomid}).findOne((err,doc)=> {
     if(doc) {
@@ -20,8 +21,9 @@ const addConnectedUser = (username,roomid) =>  {
       }
     } else {
     }
-  })
+  });
 }
+
 const removeConnectedUser = (username,roomid) =>  {
   Party.where({_id : roomid}).findOne((err,doc)=> {
     if(doc) {
@@ -29,15 +31,17 @@ const removeConnectedUser = (username,roomid) =>  {
       doc.save();
     } else {
     }
-  })
+  });
 }
+
 const sendPrevPartyMessages = (roomid, callback) =>  {
   Message.where({party : roomid}).find((err,docs)=> {
     if(docs) {
       callback(null, docs);
     }
-  })
+  });
 }
+
 const sendPartyInfo = (roomid, callback) =>  {
   Party.where({_id : roomid}).findOne((err,doc)=> {
     if(doc) {
@@ -45,25 +49,25 @@ const sendPartyInfo = (roomid, callback) =>  {
     } else {
       callback(err,null);
     }
-  })
+  });
 }
 export function setupSocketHandlers(io) {
   io.on('connection', (socket) => {
     /*
-    socket data fields 
+    socket data fields
     socket.data.current_party  // is the party id in the db
     socket.data.userid // user id in the db
     */
-    
+
     console.log('a user connected ' + socket.id);
     socket.on("disconnecting", (reason) => {
       io.to(socket.data.current_party).emit('user-left',socket.data.user);
       removeConnectedUser(socket.data.user,socket.data.current_party);
       console.log( socket.data.user+' disconnected');
     });
-    
+
     socket.on('sign-in',(session) =>{
-      // take session information and extract user id 
+      // take session information and extract user id
       let res = verifyJwt(session.token);
       if(res.valid) {
         socket.data.user = res.decoded.username;
@@ -74,10 +78,10 @@ export function setupSocketHandlers(io) {
     socket.on('join-room',(roomdata)=> {
       console.log("user requests to join "+ roomdata.roomname);
       if(socket.data.user && roomdata.roomname) // do real sanitization on these fields
-        checkUserInvited(socket.data.user,roomdata.roomname,(err, res) =>{ 
+        checkUserInvited(socket.data.user,roomdata.roomname,(err, res) =>{
           if(res) {
             console.log("user joins room " + roomdata.roomname);
-            
+
             socket.join(roomdata.roomname);
             socket.data.current_party = roomdata.roomname;
             addConnectedUser(socket.data.user,roomdata.roomname);
@@ -92,8 +96,6 @@ export function setupSocketHandlers(io) {
             });
           }
         });
-      
-
     });
 
     socket.on('send',(content)=> {
@@ -104,8 +106,6 @@ export function setupSocketHandlers(io) {
             io.to(socket.data.current_party).emit('receive',new_message);
           });
       }
-
     });
-
   });
 }
