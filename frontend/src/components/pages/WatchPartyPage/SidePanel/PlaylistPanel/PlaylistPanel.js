0b2246f91b6dd8b7c6127a702c3b6e287db1d2d2
Emile Li <37230357+emile29@users.noreply.github.com>
2022-03-19 19:18:58 -0400

    small playlist fix

--- frontend/src/components/pages/WatchPartyPage/SidePanel/PlaylistPanel/PlaylistPanel.js
+++ frontend/src/components/pages/WatchPartyPage/SidePanel/PlaylistPanel/PlaylistPanel.js
@@ -5,30 +5,26 @@ import { getSocket } from 'components/utils/socket_utils';
 import * as videoUtils from 'components/utils/video_utils';
 
 export default function PlaylistPanel({playlistData}) {
-    const [list, setList] = useState(playlistData.list);
-    const [currentIdx, setCurrentIdx] = useState(playlistData.currentIdx);
 
     const removeFromList = (index) => {
-        let temp = [...list];
+        let temp = [...playlistData.list];
         temp.splice(index, 1);
-        setList(temp);
         getSocket().emit('update-playlist', temp);
     }
 
     const changeVideo = (index) => {
-        setCurrentIdx(index);
         getSocket().emit('update-index', index);
     }
 
     return (
         <div className='playlist-panel'>
             <div className='header'>Queue 
-                {list?.length > 0 && <span> ({currentIdx+1}/{list.length})</span>}
+                {playlistData.list?.length > 0 && <span> ({playlistData.currentIdx+1}/{playlistData.list.length})</span>}
             </div>
-            {list?.length > 0 &&
-                list.map((link, index) => {
+            {playlistData.list?.length > 0 &&
+                playlistData.list.map((link, index) => {
                     return (
-                        <div key={index} className={currentIdx === index ? 'playlist-item-wrapper-current' : 'playlist-item-wrapper'}>
+                        <div key={index} className={playlistData.currentIdx === index ? 'playlist-item-wrapper-current' : 'playlist-item-wrapper'}>
                             <div className='item' onClick={()=>changeVideo(index)}><img className='thumbnail' src={videoUtils.getVideoThumbnail(link)} /></div>
                             <div className='item-delete-btn'>
                                 <ClearIcon onClick={()=>removeFromList(index)} />
