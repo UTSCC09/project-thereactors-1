524b52a67262301b01961bf725fd880910c14c61
Victor Ko <vk07275@gmail.com>
2022-03-21 02:44:59 -0400

    changed name

--- frontend/src/environments.js
+++ frontend/src/environments.js
@@ -1,5 +1,5 @@
 export default {
-  backendUrl: "https://secure-sea-81692.herokuapp.com",
-  graphqlUrl: "https://secure-sea-81692.herokuapp.com/api/graphql",
+  backendUrl: "https://yt-watchparty.herokuapp.com",
+  graphqlUrl: "https://yt-watchparty.herokuapp.com/api/graphql",
   API_KEY: "AIzaSyBnQYcaj09zjH_6qY1vHuMBPGcNk0Dw7aw"
 }
