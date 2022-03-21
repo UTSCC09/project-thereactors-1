35fc4bbb811465f9d5414ff4c87129f1fb185303
Victor Ko <vk07275@gmail.com>
2022-03-20 23:43:59 -0400

    api key issues

--- frontend/src/environments.js
+++ frontend/src/environments.js
@@ -1,4 +1,5 @@
 export default {
   backendUrl: "https://secure-sea-81692.herokuapp.com",
   graphqlUrl: "https://secure-sea-81692.herokuapp.com/api/graphql",
+  API_KEY: "AIzaSyBnQYcaj09zjH_6qY1vHuMBPGcNk0Dw7aw"
 }
