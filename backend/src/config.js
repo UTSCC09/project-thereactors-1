6796f5c01865cc7b067deea0f72159a67fab786e
Daniel Xu <niellxu@yahoo.ca>
2022-03-25 14:31:04 -0400

    Update mongodb url

--- backend/src/config.js
+++ backend/src/config.js
@@ -10,12 +10,12 @@ export const development = {
 
 export const production = {
   "port": 3001,
-  "mongodbUrl":"mongodb+srv://admin:TDIu5HUR126xwjIV@cluster0.xkzw0.mongodb.net/recipeCentral?retryWrites=true&w=majority&authSource=admin",
+  "mongodbUrl": "mongodb://admin:ytwatchparty@localhost:27020/ytwatchparty?authSource=admin",
   "jwtSecret": "bWNYDRHFFHrndh4qK7dgS7ky5DpX3EeC",
   "passwordSaltRounds": 10,
   "cookieMaxAge": 604800000,
   "API_KEY":"AIzaSyDFF4pfMtCwFrz_jGe2z73OWSH6ZK9KIVI",
-  "frontendUrl":"https://yt-watchparty.herokuapp.com/"
+  "frontendUrl":"https://ytwatch.party"
 }
 
 /**
